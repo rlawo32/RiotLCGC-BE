b9c42b1abea730656bfb8a836122e85722420189
sungjae <92013056+rlawo32@users.noreply.github.com>
2025-11-06 15:49:13 +0900

    [RiotLCGC][BE] <Discord> Discord_Bot_기능_작업(supabase 조회, embed 생성)_20251106 (4)

--- discord/discord-bot.js
+++ discord/discord-bot.js
@@ -1,6 +1,21 @@
 require('dotenv').config();
 const { Client, GatewayIntentBits } = require('discord.js');
 
+const supabase = require('./supabase.js');
+
+const getMatchSearchData = async(target) => {
+	const { data, error } = await supabase
+		.rpc('match_search', {
+          search_nickname: target
+        });
+	if (error) {
+		console.error('Error fetching data:', error);
+	} else {
+		//console.log('Data:', data);
+	}
+	return data; 
+};
+
 const client = new Client({
     intents: [
       GatewayIntentBits.Guilds, 
@@ -14,9 +29,9 @@ client.once('ready', () => {
 
     client.user.setPresence({
       activities: [
-        { name: '/record', type: 0 } // type 0 = Playing
+        { name: '/record', type: 0 }
       ],
-      status: 'online', // 온라인 상태
+      status: 'online',
     });
 });
 
@@ -35,7 +50,29 @@ client.on('interactionCreate', async (interaction) => {
     if (!interaction.isChatInputCommand()) return;
     if (interaction.commandName === 'record') {
       const nickname = interaction.options.getString('nickname');
-      await interaction.reply(`${nickname}님의 기록을 불러왔습니다!`);
+      if (!nickname) return interaction.reply('닉네임을 입력해주세요.');
+
+      const data = await getMatchSearchData(nickname);
+      if (!data || data.length === 0) return interaction.reply('플레이어 정보가 존재하지 않습니다.');
+
+      const searchData = data[0];
+      const embed = new EmbedBuilder()
+          .setColor(4321431)
+          .setTitle(searchData.lcg_nickname)
+          .setDescription('')
+          .setUrl('https://rabbitgang.vercel.app')
+          .setThumbnail(`${searchData.lcg_main_image}profileicon/${searchData.lcg_summoner_icon}`)
+          .setAuthor({ name: 'TEST', url: 'https://discord.com', iconURL: 'https://cdn.discordapp.com/embed/avatars/0.png' })
+          .addFields(
+            { name: '개인랭크', value: `**\`${searchData.lcg_present_tier} ${searchData.lcg_present_division}\`**`, inline: false },
+            { name: '게임 횟수', value: `\`${searchData.lcg_count_play}회\``, inline: true },
+            { name: 'MVP 횟수', value: `\`${searchData.lcg_count_mvp}\` (**${searchData.rankMvp}위**)`, inline: true },
+            { name: 'ACE 횟수', value: `\`${searchData.lcg_count_ace}\` (**${searchData.rankAce}위**)`, inline: true },
+          )
+          .setFooter({ text: '' })
+          .setTimestamp();
+        
+      await interaction.reply({ embeds: [embed] });
     }
 });
 
